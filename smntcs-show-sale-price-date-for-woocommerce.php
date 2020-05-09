8ad6dcf0a6a4dd9969a34e846c80bc954dc9ab57
Niels Lange <info@nielslange.de>
2020-05-09 18:00:26 +0700

    #15: Declaring required and supported Woo version

--- smntcs-show-sale-price-date-for-woocommerce.php
+++ smntcs-show-sale-price-date-for-woocommerce.php
@@ -9,8 +9,10 @@
  * Domain Path: /languages
  * Version: 1.2
  * Requires at least: 5.3
- * Tested up to: 5.4
  * Requires PHP: 5.6
+ * Tested up to: 5.4
+ * WC requires at least: 3.0
+ * WC tested up to: 4.1
  * License: GPL3+
  * License URI: http://www.gnu.org/licenses/gpl.txt
  *

78118733a0e8e892125d60bd893475a1b8fb281c
Niels Lange <info@nielslange.de>
2022-10-08 14:20:09 +0700

    Refactor plugin

--- README.txt
+++ README.txt
@@ -1,15 +1,15 @@
 === SMNTCS Show Sale Price Date for WooCommerce ===
 
-Contributors: nielslange
-Tags: WooCommerce Sale Price
-Version: 1.4
-Tested up to: 5.8
-Requires at least: 5.3
-Requires PHP: 5.6
+Contributors: 		  nielslange
+Tags: 				  WooCommerce Sale Price
+Version: 			  1.5
+Tested up to: 		  6.0
+Requires at least: 	  5.3
+Requires PHP: 		  5.6
 WC requires at least: 3.0
-WC tested up to: 6.0
-License: GPLv2+
-License URI: https://opensource.org/licenses/GPL-2.0
+WC tested up to: 	  6.9
+License: 			  GPLv3+
+License URI: 		  https://www.gnu.org/licenses/gpl-3.0.en.html
 
 Show WooCommerce sale prices date on shopping page.
 
@@ -21,16 +21,16 @@ Show WooCommerce sale prices date on shopping page.
 
 **Adjust date format:**
 
-    add_filter(‘sale_date_format’,’my_custom_sale_date_format’ );
+    add_filter( 'sale_date_format', 'my_custom_sale_date_format' );
     function my_custom_sale_date_format() {
-        return “r”;
+        return 'r';
     }
 
 **Adjust label:**
 
-    add_filter(‘sale_date_label’,’my_custom_sale_date_label’ );
+    add_filter( 'sale_date_label', 'my_custom_sale_date_label' );
     function my_custom_sale_date_label() {
-        return “Valid until”;
+        return 'Valid until';
     }
 
 == Contribute ==
@@ -44,23 +44,33 @@ Contributions are always welcome. Simply head over to [Github](https://github.co
 
 == Change log ==
 
+= 1.5 (2022.10.08) =
+
+- Test up to WC 6.9
+- Test up to WP 6.0
+
 = 1.4 (2022.01.09) =
-* Test up to WP 5.9
+
+- Test up to WP 5.9
 
 = 1.3 (2020.05.09) =
-* [Add info that WooCommerce plugin is required](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/19)
-* [Add settings link to plugin page](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/18)
-* [Declaring required and supported WooCommerce version](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/15)
-* [Make label editable via customizer](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/17)
+
+- [Add info that WooCommerce plugin is required](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/19)
+- [Add settings link to plugin page](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/18)
+- [Declaring required and supported WooCommerce version](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/15)
+- [Make label editable via customizer](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/17)
 
 = 1.2 (2020.03.21) =
-* [Add GPL3 license](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/11)
-* [Format filter on README.txt in pseudo-markdown](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/9)
-* [Update screenshot](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/10)
+
+- [Add GPL3 license](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/11)
+- [Format filter on README.txt in pseudo-markdown](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/9)
+- [Update screenshot](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/10)
 
 = 1.1 (2020.03.21) =
-* [Add build tools](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/1)
-* [Add release workflow and assets](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/2)
+
+- [Add build tools](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/1)
+- [Add release workflow and assets](https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce/issues/2)
 
 = 1.0 (2020.03.14) =
-* Initial release
+
+- Initial release
--- smntcs-show-sale-price-date-for-woocommerce.php
+++ smntcs-show-sale-price-date-for-woocommerce.php
@@ -1,135 +1,146 @@
 <?php
 /**
- * Plugin Name: SMNTCS Show Sale Price Date for WooCommerce
- * Plugin URI: https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce
- * Description: Show WooCommerce sale prices date on shopping page
- * Author: Niels Lange
- * Author URI: https://nielslange.de/
- * Text Domain: smntcs-show-sale-price-date-for-woocommerce
- * Version: 1.4
- * Tested up to: 5.8
- * Requires at least: 5.3
- * Requires PHP: 5.6
+ * Plugin Name:          SMNTCS Show Sale Price Date for WooCommerce
+ * Plugin URI:           https://github.com/nielslange/smntcs-show-sale-price-date-for-woocommerce
+ * Description:          Show WooCommerce sale prices date on shopping page
+ * Text Domain:          smntcs-show-sale-price-date-for-woocommerce
+ * Version:              1.5
+ * Tested up to:         6.0
+ * Requires at least:    5.3
+ * Requires PHP:         5.6
  * WC requires at least: 3.0
- * WC tested up to: 6.0
- * License: GPLv2+
- * License URI: https://opensource.org/licenses/GPL-2.0
+ * WC tested up to:      6.9
+ * Author:               Niels Lange
+ * Author URI:           https://nielslange.com/
+ * License:              GPLv3+
+ * License URI:          https://www.gnu.org/licenses/gpl-3.0.en.html
  *
- * @category   Plugin
- * @package    WordPress
- * @subpackage SMNTCS Show Sale Price Date for WooCommerce
- * @author     Niels Lange <info@nielslange.de>
- * @license    https://opensource.org/licenses/GPL-2.0 GNU General Public License version 2
+ * @package SMNTCS_Show_Sale_Price_Date_For_WC
  */
 
-// Avoid direct plugin access.
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Show warning if WooCommerce is not active or WooCommerce version < 3.0
- *
- * @return void
- * @since 1.3.0
+ * SMNTCS_Show_Sale_Price_Date_For_WC main class.
  */
-function smntcs_sale_price_admin_notices() {
-	global $woocommerce;
+class SMNTCS_Show_Sale_Price_Date_For_WC {
 
-	if ( ! class_exists( 'WooCommerce' ) || version_compare( $woocommerce->version, '3.0', '<' ) ) {
-		$class   = 'notice notice-warning is-dismissible';
-		$message = __( 'SMNTCS Show Sale Price Date for WooCommerce requires at least WooCommerce 3.0', 'smntcs-show-sale-price-date-for-woocommerce' );
-
-		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+	/**
+	 * Initialise the plugin.
+	 *
+	 * @return void
+	 * @since 1.2.0
+	 */
+	public static function init() {
+		add_action( 'admin_notices', array( __CLASS__, 'admin_notices' ) );
+		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( __CLASS__, 'add_plugin_settings_link' ) );
+		add_filter( 'woocommerce_get_price_html', array( __CLASS__, 'get_price_html' ), 100, 2 );
+		add_action( 'customize_register', array( __CLASS__, 'enhance_customizer' ) );
 	}
-}
-add_action( 'admin_notices', 'smntcs_sale_price_admin_notices' );
 
-/**
- * Add settings link on plugin page.
- *
- * @param array $links The original array with customizer links.
- * @return array $links The updated array with customizer links.
- * @since 1.3.0
- */
-function smntcs_sale_price_settings_link( $links ) {
-	$admin_url     = admin_url( 'customize.php?autofocus[section]=smntcs_sale_price_section' );
-	$settings_link = '<a href="' . $admin_url . '">' . __( 'Settings', 'smntcs-show-sale-price-date-for-woocommerce' ) . '</a>';
-	array_unshift( $links, $settings_link );
+	/**
+	 * Show warning if WooCommerce is not active or WooCommerce version < 3.0
+	 *
+	 * @return void
+	 * @since 1.3.0
+	 */
+	public static function admin_notices() {
+		global $woocommerce;
 
-	return $links;
-}
-add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'smntcs_sale_price_settings_link' );
+		if ( ! class_exists( 'WooCommerce' ) || version_compare( $woocommerce->version, '3.0', '<' ) ) {
+			$class   = 'notice notice-warning is-dismissible';
+			$message = __( 'SMNTCS Show Sale Price Date for WooCommerce requires at least WooCommerce 3.0', 'smntcs-show-sale-price-date-for-woocommerce' );
 
-/**
- * Add sale date end date to product page.
- *
- * @param String $price The price string.
- * @param Object $product The product object.
- * @return String The formated price.
- * @since 1.0.0
- */
-function smntcs_woocommerce_get_price_html( $price, $product ) {
-	global $post;
-	$sales_price_to = get_post_meta( $post->ID, '_sale_price_dates_to', true );
-	if ( is_single() && '' !== $sales_price_to ) {
-		$format = apply_filters( 'sale_date_format', get_option( 'date_format' ) );
-		$label  = apply_filters( 'sale_date_label', get_option( 'smntcs_sale_price_label', 'Discounted until' ) );
-		$date   = wp_date( $format, $sales_price_to );
-
-		if ( $label ) {
-			return str_replace( '</ins>', '</ins> <small>(' . esc_html( $label ) . ' ' . esc_html( $date ) . ')</small>', $price );
-		} else {
-			return str_replace( '</ins>', '</ins> <small>(' . esc_html( $date ) . ')</small>', $price );
+			printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 		}
-	} else {
-		return apply_filters( 'woocommerce_get_price', $price );
-	}
-}
-add_filter( 'woocommerce_get_price_html', 'smntcs_woocommerce_get_price_html', 100, 2 );
-
-/**
- * Enhance WordPress customizer.
- *
- * @param WP_Customize_Manager $wp_customize The customizer object.
- * @return void
- * @since 1.3.0
- */
-function smntcs_sale_price_enhance_customizer( $wp_customize ) {
-	global $woocommerce;
-
-	// Return if WooCommerce hasn't been installed.
-	if ( ! class_exists( 'WooCommerce' ) ) {
-		return;
 	}
 
-	// Create customizer section.
-	$wp_customize->add_section(
-		'smntcs_sale_price_section',
-		array(
-			'title'    => __( 'Show Sale Price Date', 'smntcs-show-sale-price-date-for-woocommerce' ),
-			'priority' => 50,
-			'panel'    => 'woocommerce',
-		)
-	);
+	/**
+	 * Add settings link on plugin page.
+	 *
+	 * @param array $links The original array with customizer links.
+	 * @return array $links The updated array with customizer links.
+	 * @since 1.3.0
+	 */
+	public static function add_plugin_settings_link( $links ) {
+		$admin_url     = admin_url( 'customize.php?autofocus[section]=smntcs_sale_price_section' );
+		$settings_link = '<a href="' . $admin_url . '">' . __( 'Settings', 'smntcs-show-sale-price-date-for-woocommerce' ) . '</a>';
+		array_unshift( $links, $settings_link );
 
-	// Label "Discounted until".
-	$wp_customize->add_setting(
-		'smntcs_sale_price_label',
-		array(
-			'default'           => 'Discounted until',
-			'sanitize_callback' => 'sanitize_text_field',
-			'type'              => 'option',
-		)
-	);
-	$wp_customize->add_control(
-		'smntcs_sale_price_label',
-		array(
-			'label'       => __( 'Label', 'smntcs-show-sale-price-date-for-woocommerce' ),
-			'section'     => 'smntcs_sale_price_section',
-			'type'        => 'text',
-			'input_attrs' => array(
-				'placeholder' => __( 'Discounted until', 'smntcs-show-sale-price-date-for-woocommerce' ),
-			),
-		)
-	);
+		return $links;
+	}
+
+	/**
+	 * Add sale date end date to product page.
+	 *
+	 * @param String $price The price string.
+	 * @param Object $product The product object.
+	 * @return String The formated price.
+	 * @since 1.0.0
+	 */
+	public static function get_price_html( $price, $product ) {
+		global $post;
+		$sales_price_to = get_post_meta( $post->ID, '_sale_price_dates_to', true );
+		if ( is_single() && '' !== $sales_price_to ) {
+			$format = apply_filters( 'sale_date_format', get_option( 'date_format' ) );
+			$label  = apply_filters( 'sale_date_label', get_option( 'smntcs_sale_price_label', 'Discounted until' ) );
+			$date   = wp_date( $format, $sales_price_to );
+
+			if ( $label ) {
+				return str_replace( '</ins>', '</ins> <small>(' . esc_html( $label ) . ' ' . esc_html( $date ) . ')</small>', $price );
+			} else {
+				return str_replace( '</ins>', '</ins> <small>(' . esc_html( $date ) . ')</small>', $price );
+			}
+		} else {
+			return apply_filters( 'woocommerce_get_price', $price );
+		}
+	}
+
+	/**
+	 * Enhance WordPress customizer.
+	 *
+	 * @param WP_Customize_Manager $wp_customize The customizer object.
+	 * @return void
+	 * @since 1.3.0
+	 */
+	public static function enhance_customizer( $wp_customize ) {
+		global $woocommerce;
+
+		// Return if WooCommerce hasn't been installed.
+		if ( ! class_exists( 'WooCommerce' ) ) {
+			return;
+		}
+
+		$wp_customize->add_section(
+			'smntcs_sale_price_section',
+			array(
+				'title'    => __( 'Show Sale Price Date', 'smntcs-show-sale-price-date-for-woocommerce' ),
+				'priority' => 50,
+				'panel'    => 'woocommerce',
+			)
+		);
+
+		$wp_customize->add_setting(
+			'smntcs_sale_price_label',
+			array(
+				'default'           => 'Discounted until',
+				'sanitize_callback' => 'sanitize_text_field',
+				'type'              => 'option',
+			)
+		);
+
+		$wp_customize->add_control(
+			'smntcs_sale_price_label',
+			array(
+				'label'       => __( 'Label', 'smntcs-show-sale-price-date-for-woocommerce' ),
+				'section'     => 'smntcs_sale_price_section',
+				'type'        => 'text',
+				'input_attrs' => array(
+					'placeholder' => __( 'Discounted until', 'smntcs-show-sale-price-date-for-woocommerce' ),
+				),
+			)
+		);
+	}
 }
-add_action( 'customize_register', 'smntcs_sale_price_enhance_customizer' );
+
+SMNTCS_Show_Sale_Price_Date_for_WC::init();
